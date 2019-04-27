84a3696e1793b8c9a488f7cbb6e49ae3aec02acd
Anais Trope <trope.anais@gmail.com>
2019-04-27 21:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,3 @@ function happyHolidayTo(holiday, name){
 }
 
 /*Write a function named holidayCountdown with two parameters. */
-function holidayCountDown(holiday, days){
-  return `It's ${days} days until ${holiday}!`
-}
