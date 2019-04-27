3fe22eccd24d3fd33bc7975d0acdd41e6094bec9
Anais Trope <trope.anais@gmail.com>
2019-04-27 21:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function happyHolidays(){
 
 /*Write a function named happyHolidaysTo with one parameter*/
 function happyHolidaysTo(name){
-  return (`Happy holidays, ${name}`)
+  return `Happy holidays, ${name}`
 }
 happyHolidaysTo("Sonia")
 
 /*Write a function named happyHolidayTo with two parameters*/
 function happyHolidaytTo(holiday, name){
-  return (`Happy ${holiday}, ${name}`)
+  return `Happy ${holiday}, ${name}`
 }
 happyHolidayTo("Easter", "Sonia")
