8d03b05be1ba02484fd17b58fa2b2c9d249aa951
Anais Trope <trope.anais@gmail.com>
2019-04-27 21:25:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // write your code below!
 
-//
+/*Write a function named happyHolidays returning the string "Happy holidays" */
 function happyHolidays(){
   return ("Happy holidays!")
 }
+
+/*Write a function named happyHolidaysTo with a parameter returning a greeting */
+function happyHolidaysTo(name){
+  return (`Happy holidays, ${name}`)
+}
+happyHolidaysTo("Sonia")
+
 
